f30cded54e073accee926d4240b32f39f072cfa0
Maciek <msitkowski94@gmail.com>
2022-08-06 09:34:49 +0200

    finish guessing game

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ cargo run --example hello-world
 ```
 
 [Add examples to your Rust libraries](http://xion.io/post/code/rust-examples.html)
+
+## Resources
+
+- [The Rust Programming Language](https://doc.rust-lang.org/book/title-page.html)
+  - [x] Getting Started
+    - [x] Hello, World!
+    - [x] Hello, Cargo!
+  - [x] Programming a Guessing Game
---
+++ examples/guessing-game.rs
@@ -0,0 +1,35 @@
+use colored::Colorize;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{}", "Please enter a number!".bold().red());
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("{}", "You win!".bold().green());
+                break;
+            }
+        }
+    }
+}
